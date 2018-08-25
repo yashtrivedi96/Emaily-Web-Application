88a9da9422cc4ced179a89056759654809403595
yashtrivedi96 <yashtrivedi618@gmail.com>
2018-08-25 14:12:08 +0530

    add requireLogin to surveyRoutes.js

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys", async (req, res) => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
     const surveys = await Survey.find({ _user: req.user.id }).select({
       recipients: false
     });
